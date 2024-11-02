7fb11730243b209ac8e547428581274e430402d2
Tyler Finethy <tylfin@gmail.com>
2024-11-02 13:17:50 -0400

    Fix: Update pyodide to latest v0.26.3 (#129)

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
@@ -13,13 +14,27 @@ module.exports = {
   resolve: {
     alias: {
       'node-fetch': 'axios',
-    }
+    },
+    fallback: {
+      // Ensure that the standard modules are also set to false
+      'fs': false,
+      'path': false,
+      'url': false,
+      'crypto': false,
+      'vm': false,
+      'child_process': false,
+    },
   },
   optimization: {
     minimize: true,
     splitChunks: false,
     runtimeChunk: false,
   },
+  plugins: [
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^node:/ // Ignores imports starting with 'node:'
+    })
+  ],
   module: {
     rules: [
       {
